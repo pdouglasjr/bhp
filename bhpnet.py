855c73121709dbec93d3c088370b419b66f84082
Phillip D. Douglas, Jr <phillip.duane.douglas.jr@gmail.com>
2020-04-11 21:01:24 -0400

    Updates.

--- bhpnet.py
+++ bhpnet.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import sys
 import socket
 import getopt
@@ -23,8 +25,34 @@ def usage():
     print "-c --command\t\t\t- initialize a command shell"
     print "-u --upload=destination\t\t- upon receiving connection, upload a\n\t\t\t\t  file and write to [destination]"
     print
-    print
     print "Examples:"
-    print "bhpnet.py "
+    print "  bhpnet.py -t 192.168.0.1 -p 5555 -l -c"
+    print "  bhpnet.py -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe"
+    print "  bhpnet.py -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\""
+    print "  echo 'ABCDEFGHI' | ./bhpnet.py -t 192.168.11.12 -p 135"
+    sys.exit(0)
 
-usage()
+def main():
+    global listen
+    global port
+    global execute
+    global command
+    global upload_destination
+    global target
+
+    if not len(sys.argv[1:]):
+        usage()
+
+    # read the commandline options
+    try:
+        opts,args = getopt.getopt(
+                sys.argv[1:],
+                "hle:t:p:cu:", 
+                ["help","listen","execute","target","port","command","upload"])
+    except getopt.GetoptError as err:
+        print str(err)
+        usage()
+
+    print opts
+
+main()
